95c31c8ec6bef235538a511a56c17a2cca17eae0
Okoromi Victor <okoromivictorsunday@gmail.com>
2020-10-03 21:54:09 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map'
+import { Album } from './album'
 
 @Injectable()
 
@@ -12,7 +13,8 @@ export class ProductService {
 
     getAlbum(id: number) {
         return this._http.get(this._albumUrl).map((response) => 
-            response.json());
+            response.json(<Album>));
+            
     }
 
 }
