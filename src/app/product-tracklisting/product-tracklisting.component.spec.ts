235c948282e92db53f32b8de6eea58be792a36dd
Okoromi Victor <okoromivictorsunday@gmail.com>
2020-10-03 23:08:22 +0100

    Completed module

---
+++ src/app/product-tracklisting/product-tracklisting.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductTrackListing } from './product-tracklisting.component';
+
+describe('ProductPageComponent', () => {
+  let component: ProductTrackListing;
+  let fixture: ComponentFixture<ProductDescriptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductTrackListing ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductTrackListing);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
