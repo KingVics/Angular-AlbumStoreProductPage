735c04776bbcedec939e29ea37fd271a92dce48d
Okoromi Victor <okoromivictorsunday@gmail.com>
2020-10-04 13:28:55 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ import { Product } from "./product"
 export class ProductService {
 
     private _albumUrl = '../assets/album.json';
-    private _productsUrl = '../assets/product.json'
+    private _productsUrl = '../assets/products.json'
 
     constructor(private _http: Http) {}
 
@@ -21,7 +21,7 @@ export class ProductService {
             
     }
 
-    getProducts() {
+    getProducts(): Observable<Product[]> {
         return this._http.get(this._productsUrl).map(response => 
            <Product[]>response.json());
     }
